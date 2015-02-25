e7b1706c665ed75f540860087c8d81a959d68083
Sárközi Viktor <s.wictor898@gmail.com>
2015-02-25 17:50:21 +0100

    A 3-al osztható számokra "fizz"-t ír!

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,7 +3,12 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
     
     public String execute(int number) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (int i = 0; i < 100; i++) {
+            if(i%3==0){
+                return "fizz";
+            }
+        }
+        throw new UnsupportedOperationException();
     }
     
 }
