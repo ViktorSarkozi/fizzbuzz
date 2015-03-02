900c59be619d96ee99feb1932975c66cba480541
ViktorSarkozi <s.wictor898@gmail.com>
2015-03-02 22:49:41 +0100

    A 15-el osztható számokra "fizzbuzz"-t ír!

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -1,15 +1,16 @@
 package com.wcs.fizzbuzz;
 
 public class FizzBuzzer {
-    
+
     public String execute(int number) {
-            if(number%5==0){
-                return "buzz";
-            }
-            else if(number%3==0){
-                return "fizz";
-            }
+        if (number % 15 == 0) {
+            return "fizzbuzz";
+        } else if (number % 5 == 0) {
+            return "buzz";
+        } else if (number % 3 == 0) {
+            return "fizz";
+        }
         throw new UnsupportedOperationException();
     }
-    
+
 }
